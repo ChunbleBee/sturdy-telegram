deb7f6b981c478b06fa620db6a8689f0042e0b8d
ChunbleBee <tai.williams@wsu.edu>
2022-02-12 00:19:24 -0800

    All major bugs Fixed! Correctly printing a Mandelbrot set in the terminal now! 🥳

--- src/AsciiMandelbrot/Asciibrot.go
+++ src/AsciiMandelbrot/Asciibrot.go
@@ -10,29 +10,35 @@ import (
 type Pixel struct {
 	X int
 	Y int
-	Color string
+	Color *string
 	ZReal float64
 	ZComplex float64
 }
 
 // TODO: Create the map of color codes
-var ColorMap = map[int]string {}
+var ColorMap = []string{"40","41","42","43","44","45","46","47","48","49","50"}
+
 var MaxIters int = 0
 var XScale float64 = 0.0
 var YScale float64 = 0.0
 
-func (this Pixel) getColor(iters int) {
-	fmt.Println("in getColor()")
-	this.Color = ColorMap[int(math.Round((float64(iters)/float64(MaxIters))*10))]
+func (this *Pixel) getColor(iters int) {
+	this.Color = &ColorMap[ int( math.Floor((float64(iters) / float64(MaxIters)) * 10) )]
 }
 
-func (this Pixel) getZValues() {
-	fmt.Println("in getZValues()", this.X, this.Y)
-	curIter := 0
+func qsquare(base float64) float64 {
+	return base * base
+}
 
-	for ; (math.Pow(this.ZReal, 2.0) + math.Pow(this.ZComplex, 2.0) <= 4.0) && (curIter < MaxIters); curIter++ {
-		nextZReal := math.Pow(this.ZReal, 2.0) - math.Pow(this.ZComplex, 2.0) + float64(this.X)*YScale - 2
-		nextZComplex := 2 * this.ZReal * this.ZComplex + float64(this.Y)*YScale - 2
+func (this *Pixel) getZValues() {
+	curIter := 0
+	
+	zReal0 := float64(this.X)*XScale - 2
+	zComplex0 := float64(this.Y) * YScale - 2
+
+	for ; (qsquare(this.ZReal) + qsquare(this.ZComplex) <= 4.0) && (curIter < MaxIters); curIter++ {
+		nextZReal := qsquare(this.ZReal) - qsquare(this.ZComplex) + zReal0
+		nextZComplex := 2 * this.ZReal * this.ZComplex + zComplex0
 
 		this.ZReal = nextZReal
 		this.ZComplex = nextZComplex
@@ -41,24 +47,22 @@ func (this Pixel) getZValues() {
 	this.getColor(curIter)
 }
 
-// TODO: Make this a debug function, 
-// Create a string function to only create a new string with a background color code, a space character, then a console-color default code.
+// TODO:
 func (this Pixel) String() string {
-	return fmt.Sprintf("(%d, %d), (%f, %f), Color:%s ", this.X, this.Y, this.ZReal, this.ZComplex, this.Color)
+	return fmt.Sprintf("\033[%sm \033[0m", *this.Color)
 }
 
 // TODO: Add a thread-block before starting to print all the pixel info.
 // TODO: add the multithreading back in, & limit logical threads to number of CPU threads.
-// TODO: Fix for each Pixel printing.
 // TODO: Make the cmdline arg parsing better.
-// TODO: more todos
 func main() {
 	fmt.Println("")
 	cmdArgs := os.Args[1:]
 
 	xSize, ex := strconv.Atoi(cmdArgs[0])
 	ySize, ey := strconv.Atoi(cmdArgs[1])
-	MaxIters, ei := strconv.Atoi(cmdArgs[2])
+	var ei error = nil; // Fixes scoping problem for MaxIters
+	MaxIters, ei = strconv.Atoi(cmdArgs[2])
 	
 	if (ex != nil || ey != nil || ei != nil) {
 		if (ex != nil) {
@@ -76,7 +80,7 @@ func main() {
 		fmt.Println("Exiting...")
 		return
 	}
-	fmt.Printf("XSize: %d, YSize: %d, Iterations per Pixel: %d\n", xSize, ySize, MaxIters)
+
 	pixels := make([]Pixel, xSize*ySize)
 	// x/xtot = mx/mtot, where mtot = 4 (range of mandelbrot set)
 	// -> x * mtot/xtot = mx
@@ -86,22 +90,21 @@ func main() {
 	// Thus, we get:
 	// zreal_0 = (x * mtot/xtot) - 2
 	// This follows for y and zcomplex_0
-	fmt.Println("past array creation")
 	XScale = 4.0/float64(xSize)
 	YScale = 4.0/float64(ySize)
-
-	fmt.Println("past scale calc")
 
 	for y := 0; y < ySize; y++ {
 		for x := 0; x < xSize; x++ {
 			index := x + y*xSize;
-			fmt.Printf("(%d, %d)\n", x, y)
-			pixels[index] = Pixel{x, y, "", 0.0, 0.0}
+			pixels[index] = Pixel{x, y, &ColorMap[0], 0.0, 0.0}
 			pixels[index].getZValues()
 		}
 	}
 
 	for _, pix := range pixels {
-		fmt.Println(pix)
+		fmt.Printf("%s", pix)
+		if pix.X == xSize - 1 {
+			fmt.Print("\n")
+		}
 	}
 }
